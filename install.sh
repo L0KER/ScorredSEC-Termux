c2551f3e83c9e181581c221299ff710b14c51944
L0KER <44724465+L0KER@users.noreply.github.com>
2019-02-10 21:06:14 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ clear
 
 echo "ScorredSEC installer"
 
-chmod +x /data/data/com.termux/files/home/ScorredSEC/scorredsec.py
+chmod +x /data/data/com.termux/files/home/ScorredSEC-Termux/scorredsec.py
 apt update
 apt upgrade
 apt install python
