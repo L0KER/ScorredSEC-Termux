060e044f2d1f24423c7bdb2a5676b1e48223c969
L0KER <44724465+L0KER@users.noreply.github.com>
2019-08-19 02:25:22 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,25 @@
 clear
 
-echo '''
+echo -e "\e[31m
  __               __ _ ____               
 (_  _ _ ._.__  _|(_ |_/  | _ ._._ _       
-__)(_(_)| |(/_(_|__)|_\_ |(/_| | | ||_|><   
-'''
+__)(_(_)| |(/_(_|__)|_\_ |(/_| | | ||_|><                                      
+\e[0m"
 
+echo -e "\e[1;33m[*]\e[0m Installing ScorredSEC Termux"
 chmod +x scorredsec.sh
 apt update -y
 apt upgrade -y
 
 clear
 
-echo '''
+echo -e "\e[31m
  __               __ _ ____               
 (_  _ _ ._.__  _|(_ |_/  | _ ._._ _       
-__)(_(_)| |(/_(_|__)|_\_ |(/_| | | ||_|><   
-'''
+__)(_(_)| |(/_(_|__)|_\_ |(/_| | | ||_|><                                      
+\e[0m"
 
-echo "======================"
-echo "Installation complete!"
-echo "======================"
+echo -e "\e[36m ==========================\e[0m"
+echo -e "\e[1;33m|\e[0m\e[1;33m[+]\e[0m \e[32mInstallation complete!\e[0m\e[1;33m|\e[0m"
+echo -e "\e[1;33m|\e[0m\e[1;33m[*]\e[0m Run ./scorredsec.sh   \e[1;33m|\e[0m"
+echo -e "\e[36m ==========================\e[0m"
